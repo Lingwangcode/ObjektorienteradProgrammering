87e591f9d30cb286403a3021b0eadb1de927e82e
Ling Wang <amyling.wang@gmail.com>
2022-10-06 20:52:17 +0200

    rättat till variabelnamn.

--- test/sprint1/inlämning1/VäxterTest.java
+++ test/sprint1/inlämning1/VäxterTest.java
@@ -15,20 +15,20 @@ class VäxterTest {
     @Test
     void getVätskaMängd() {
 
-        assert(igge.getVätskaMängd() == 0.2);
-        assert(igge.getVätskaMängd() != 2);
+        assert(igge.getVätskeMängd() == 0.2);
+        assert(igge.getVätskeMängd() != 2);
 
-        assert (p1.getVätskaMängd() == p1.getLängd() * 3);
-        assert (p1.getVätskaMängd() != p1.getVätskaMängd() * 2);
+        assert (p1.getVätskeMängd() == p1.getLängd() * 3);
+        assert (p1.getVätskeMängd() != p1.getVätskeMängd() * 2);
 
-        assert (p2.getVätskaMängd() == p2.getLängd() * 3);
-        assert (p2.getVätskaMängd() != p2.getLängd() * 1);
+        assert (p2.getVätskeMängd() == p2.getLängd() * 3);
+        assert (p2.getVätskeMängd() != p2.getLängd() * 1);
 
-        assert(k.getVätskaMängd() == k.getLängd() * 0.2 + 0.1);
-        assert(k.getVätskaMängd() != k.getLängd() * 0.2 + 0.2);
+        assert(k.getVätskeMängd() == k.getLängd() * 0.2 + 0.1);
+        assert(k.getVätskeMängd() != k.getLängd() * 0.2 + 0.2);
 
-        assert(k2.getVätskaMängd() == k2.getLängd() * 0.2 + 0.1);
-        assert(k2.getVätskaMängd() != k2.getLängd() * 0.5 + 0.1);
+        assert(k2.getVätskeMängd() == k2.getLängd() * 0.2 + 0.1);
+        assert(k2.getVätskeMängd() != k2.getLängd() * 0.5 + 0.1);
 
     }
 }
