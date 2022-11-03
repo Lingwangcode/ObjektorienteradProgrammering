5604d5597cb8c82ee198329db75abbf346b8c53a
Ling Wang <amyling.wang@gmail.com>
2022-11-03 13:42:07 +0100

    ändring i inlämning 3 Game

--- src/sprint3/femtonspel/Game.java
+++ src/sprint3/femtonspel/Game.java
@@ -8,7 +8,7 @@ import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Game extends JFrame implements ActionListener {
+public class Game extends JFrame implements ActionListener{
 
     JPanel buttonPanel = new JPanel();
     JButton[][] buttons = new JButton[4][4];
@@ -17,7 +17,6 @@ public class Game extends JFrame implements ActionListener {
     JPanel panel = new JPanel();
     JButton nyttSpel = new JButton("Nytt spel");
     JButton test = new JButton("Test spel");
-
 
     public Game() {
         setLayout(new BorderLayout());
@@ -37,11 +36,13 @@ public class Game extends JFrame implements ActionListener {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    public void addButtons() {
+    public void addButtons() { // Initierar knapparna
+
         Dimension d = new Dimension(50, 50);
         try {
             for (int i = 0; i < buttons.length; i++) {
                 for (int j = 0; j < buttons.length; j++) {
+
                     buttons[i][j] = new JButton();
                     buttonPanel.add(buttons[i][j]);
                     buttons[i][j].setPreferredSize(d);
@@ -54,56 +55,92 @@ public class Game extends JFrame implements ActionListener {
 
     }
 
-    public void win() {
+    public void shuffle() { //sätt nummer på knapparna
         try {
+            boolean[] isUsed = new boolean[16];
+
+            for (int i = 0; i < buttons.length; i++) {
+                for (int j = 0; j < buttons.length; j++) {
+
+                    int randomNumber = 0;
+
+                    while (isUsed[randomNumber]) {
+                        randomNumber = (int) (Math.random() * 16);
+                    }
+                    isUsed[randomNumber] = true;
+                    if (randomNumber != 0) {
+                        buttons[i][j].setText("" + randomNumber);
+                    } else {
+                        buttons[i][j].setText("");
+                        r = i;
+                        c = j;
+                    }
+
+                }
+            }
+        }catch (IndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void win() { //convertera textstring på knapparna till int och kolla ifall numren är sorterade
+        try {
+
             List<Integer> buttonNr = new ArrayList<>();
             String buttonText = "";
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
+
+            for (int i = 0; i < buttons.length; i++) {
+                for (int j = 0; j < buttons.length; j++) {
+
                     buttonText = buttons[i][j].getText();
+
                     if (buttonText != "") {
+
                         buttonLabel[i][j] = Integer.parseInt(buttonText);
                         buttonNr.add(buttonLabel[i][j]);
+
                     } else {
+
                         buttonText = "16";
                         buttonLabel[i][j] = Integer.parseInt(buttonText);
                         buttonNr.add(buttonLabel[i][j]);
 
                     }
-
                 }
             }
 
         if (isListSorted(buttonNr) == true) {
+
             JOptionPane.showMessageDialog(null,"You win!!!");
             System.exit(0);
         }
         }catch (IndexOutOfBoundsException e){
             e.printStackTrace();
         }
-
-
     }
 
-    public void sort() {
+    public void sort() { //Sorterar numren på knapparna i rätt ordning
         try{
-        int count = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                count ++;
-                buttons[i][j].setText(""+count);
+
+            int count = 0;
+            for (int i = 0; i < buttons.length; i++) {
+                for (int j = 0; j < buttons.length; j++) {
+
+                    count ++;
+                    buttons[i][j].setText(""+count);
+                }
             }
-        }
-        buttons[3][3].setText("");
-        r = 3;
-        c = 3;
+            buttons[buttons.length-1][buttons.length-1].setText("");
+            r = buttons.length - 1;
+            c = buttons.length - 1;
         }catch (IndexOutOfBoundsException e){
             e.printStackTrace();
         }
 
 
     }
-    public boolean isListSorted(List<Integer> buttonNr){
+    public boolean isListSorted(List<Integer> buttonNr){ // kollar ifall numren på knapparna är sorterade
+
         if ( buttonNr.stream().sorted().collect(Collectors.toList()).equals(buttonNr))
             return true;
         else
@@ -111,35 +148,15 @@ public class Game extends JFrame implements ActionListener {
 
     }
 
-    public void shuffle() {
-        boolean[] isUsed = new boolean[16];
-
-        for (int i = 0; i < 4; i++){
-            for (int j = 0; j< 4; j++){
-                int randomNumber = 0;
-                while (isUsed[randomNumber]){
-                    randomNumber = (int)(Math.random()*16);
-                }
-                isUsed[randomNumber] = true;
-                if (randomNumber != 0){
-                    buttons[i][j].setText("" + randomNumber);
-                }
-                else {
-                    buttons[i][j].setText("");
-                    r = i; c = j;
-                }
-
-            }
-
-        }
-
-    }
-
-    @Override
+   @Override
     public void actionPerformed(ActionEvent e) {
-        for (int i = 0; i < 4; i++){
-            for (int j = 0; j < 4; j++){
+
+        for (int i = 0; i < buttons.length; i++){
+
+            for (int j = 0; j < buttons.length; j++){
+
                 if (buttons[i][j] == e.getSource()){
+
                     if (i - 1 == r && j == c || i + 1 == r && j == c ||
                             i == r && j - 1 == c || i == r && j + 1 == c){
                         buttons[r][c].setText(buttons[i][j].getText());
@@ -159,14 +176,9 @@ public class Game extends JFrame implements ActionListener {
         if (e.getSource()== test){
             sort();
         }
-
     }
-
 
     public static void main(String[] args) {
         Game g = new Game();
-
     }
-
-
 }
